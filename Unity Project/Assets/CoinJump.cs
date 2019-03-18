8eee0cfaf0447d70a1894f1d8b74c45dc6938d6c
halimt92 <48407395+halimt92@users.noreply.github.com>
2019-03-18 20:07:05 +0200

    CoinBox

---
+++ Unity Project/Assets/CoinJump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinJump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-2, 2), Random.Range(3, 6)), ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Unity Project/Assets/_Scripts/ChestController.cs
+++ Unity Project/Assets/_Scripts/ChestController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestController : MonoBehaviour {
 
+public class ChestController : MonoBehaviour {
+    public Transform SpawnPt;
+    public GameObject Coin;
+    private bool CoinPopped = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,9 +18,13 @@ public class ChestController : MonoBehaviour {
 	}
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag=="Player")
+        if(collision.tag=="Player" && !CoinPopped)
         {
             GetComponent<Animator>().enabled = true;
+            GameObject temp=GameObject.Instantiate(Coin, SpawnPt.position, SpawnPt.rotation);
+            temp.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-2, 2), Random.Range(3, 6)), ForceMode2D.Impulse);
+
+            CoinPopped = true;
         }
     }
 }
